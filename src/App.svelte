30dcf30a945ac5838a7b618a8b2638da5b2e052d
Aditya V <mercisthebest98@gmail.com>
2021-03-30 23:09:36 +0530

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,40 @@
+<script>
+	export let name;
+
+	let rando;
+
+	const setRando = () =>{
+		rando = Math.random()
+	}
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<div>
+		{rando} a svelte random number
+	</div>
+	<button on:click={setRando}>Randomize</button>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
